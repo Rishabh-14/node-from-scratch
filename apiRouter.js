f089d40e9b37171180a967d2e5bf2c4faf429406
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-08-25 16:29:08 +0530

    ImprovedRouter.js

--- apiRouter.js
+++ apiRouter.js
@@ -1,19 +1,47 @@
-class APIRouter {
+class ImprovedAPIRouter {
   constructor() {
     this.routes = {};
   }
-  register(type, handler) {
-    this.routes[type] = handler;
+
+  // Register a route with a specific HTTP method
+  register(method, path, handler, middleware = []) {
+    method = method.toUpperCase();
+    if (!this.routes[method]) {
+      this.routes[method] = {};
+    }
+
+    if (this.routes[method][path]) {
+      throw new Error(`Route [${method} ${path}] is already registered.`);
+    }
+
+    this.routes[method][path] = {
+      handler: handler,
+      middleware: middleware,
+    };
   }
 
-  route(request) {
-    const handler = this.routes[(request, type)];
-    if (handler) {
-      return handler(request);
-    } else {
-      throw new Error("Unknown request type");
+  async route(request) {
+    const method = request.method.toUpperCase();
+    const routeInfo = this.routes[method] && this.routes[method][request.path];
+
+    if (!routeInfo) {
+      return {
+        status: 404,
+        body: "Not Found",
+      };
     }
+
+    // Run middlewares
+    for (const mw of routeInfo.middleware) {
+      const mwResult = await mw(request);
+      if (mwResult) {
+        return mwResult; // If middleware returns a result, short-circuit and return it
+      }
+    }
+
+    // Run main handler
+    return await routeInfo.handler(request);
   }
 }
 
-module.exports = new APIRouter();
+module.exports = new ImprovedAPIRouter();
