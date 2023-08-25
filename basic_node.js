8a597c87012255873f3b0ecb3ff070cd0db33747
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-08-25 10:43:42 +0530

    error fixed

--- basic_node.js
+++ basic_node.js
@@ -1,27 +1,28 @@
-const socket = require('socket')
-const threading = require('threading')
+const net = require("net");
 
-class simpleNode:
-    constructor(host,port){
-        self.host = host;
-        self.port = port;
-    }
-    
-    start(){
-        const server = net.createServer((client)=>{
-            this.handleClient(client);
-        });
-        server.listen(this.host, this.port, () => {
-            console.log(`Server started at ${this.host} ${this.port}`);
-        });
+class SimpleNode {
+  constructor(host, port) {
+    this.host = host;
+    this.port = port;
+  }
 
-        handleClient(client){
-            client.on('data',(data) => {
-                console.log(`data passed to ${data.log}`);
-                data.end();
-            });
-        }
-    }
+  start() {
+    const server = net.createServer((client) => {
+      this.handleClient(client);
+    });
 
-    const node = SimpleNode('127.0.0.1', 8080);
-    node.start();
+    server.listen(this.port, this.host, () => {
+      console.log(`Server started at ${this.host}:${this.port}`);
+    });
+  }
+
+  handleClient(client) {
+    client.on("data", (data) => {
+      console.log(`Data received: ${data.toString()}`);
+      client.end();
+    });
+  }
+}
+
+const node = new SimpleNode("127.0.0.1", 8080);
+node.start();
